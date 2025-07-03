184a91114f4790ede32f0083cabc1edb541e5375
ElianFS <eli.rivera@duocuc.cl>
2025-07-03 11:17:16 -0400

    Sumar.py

---
+++ Fucnioness/Sumar.py
@@ -0,0 +1,12 @@
+def Suma():
+  print("Suma")
+
+  try:
+    num1 = float(input("Ingresar primer numero: "))
+    num2 = float(input("ingresar segundo numero: "))
+
+    resultado = num1 + num2
+    print(f"Resultado: {num1} + {num2} = {resultado}")
+  except ValueError:
+    print("Por favor, ingrese solo numeros reales.")
+Suma()

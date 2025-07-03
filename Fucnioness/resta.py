805c80ce3e6edf3f9720dfeae83138004d652711
ElianFS <eli.rivera@duocuc.cl>
2025-07-03 11:14:57 -0400

    Update resta.py

--- Fucnioness/resta.py
+++ Fucnioness/resta.py
@@ -1,12 +1,12 @@
 def resta():
   print("Resta")
 
-   try:
-    num1 = float(input("Ingresar primer numero"))
-    num2 = float(input("ingresar segundo numero"))
+  try:
+    num1 = float(input("Ingresar primer numero: "))
+    num2 = float(input("ingresar segundo numero: "))
 
     resultado = num1 - num2
     print(f"Resultado: {num1} - {num2} = {resultado}")
-   except ValueError:
+  except ValueError:
     print("Por favor, ingrese solo numeros reales.")
 resta()
